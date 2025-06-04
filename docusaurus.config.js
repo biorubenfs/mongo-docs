deeb26bde6648f408da7aefd6b68a0f6484249ae
biorubenfs <biorubenfs@hotmail.com>
2025-06-05 01:24:33 +0200

    chore: change naming

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,12 +18,12 @@ const config = {
   url: 'https://github.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/my-website/',
+  baseUrl: '/mongo-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'biorubenfs', // Usually your GitHub org/user name.
-  projectName: 'my-website', // Usually your repo name.
+  projectName: 'mongo-docs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
